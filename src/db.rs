36121742fb81a2c228dad4a6cb14c63bb243a171
Shane Mendez <shane@themelio.org>
2023-04-28 00:22:52 -0500

    learning about why a builder would be nice

--- src/db.rs
+++ src/db.rs
@@ -5,32 +5,27 @@ use async_trait::async_trait;
 use sqlx::{pool, postgres::PgRow, Pool, Postgres};
 
 #[async_trait]
-pub trait FromDatabase<K: Clone>: Sized {
+pub trait FromDatabase: Sized {
     type ERROR: Send + Sync + 'static + Into<anyhow::Error>;
+    type Id: std::fmt::Debug + Clone;
 
-    async fn build_from_index(id: &K, pool: Pool<Postgres>) -> Result<Self, Self::ERROR>;
+    async fn build_from_index(id: &Self::Id, pool: Pool<Postgres>) -> Result<Self, Self::ERROR>;
     // async fn save_to_db(&self, pool: Pool<Postgres>) -> Result<Self::OK, Self::ERROR>;
 }
 
-impl<Struct: FromDatabase<Id>, Id: Clone> From<Id> for IndexItem<Struct, Id> {
-    fn from(id: Id) -> Self {
-        IndexItem::Builder(id)
-    }
-}
-
 #[derive(Debug, Clone)]
-pub enum IndexItem<Struct: FromDatabase<Id>, Id: Clone> {
-    Builder(Id),
+pub enum IndexItem<Struct: FromDatabase> {
+    Id(<Struct as FromDatabase>::Id),
     Value(Struct),
 }
 
-impl<Struct: FromDatabase<Id> + Clone, Id: Clone> IndexItem<Struct, Id> {
+impl<Struct: FromDatabase + Clone> IndexItem<Struct> {
     pub async fn fetch(
         &self,
         pool: sqlx::Pool<Postgres>,
-    ) -> Result<IndexItem<Struct, Id>, anyhow::Error> {
-        let a: Result<IndexItem<Struct, Id>, anyhow::Error> = match &self {
-            IndexItem::Builder(id) => {
+    ) -> Result<IndexItem<Struct>, anyhow::Error> {
+        let a: Result<IndexItem<Struct>, anyhow::Error> = match &self {
+            IndexItem::Id(id) => {
                 let k = Struct::build_from_index(id, pool)
                     .await
                     .map_err(|e| anyhow::anyhow!(e))?;
--- src/models.rs
+++ src/models.rs
@@ -13,8 +13,9 @@ pub struct User {
 }
 
 #[async_trait]
-impl FromDatabase<i32> for User {
+impl FromDatabase for User {
     type ERROR = anyhow::Error;
+    type Id = i32;
 
     async fn build_from_index(id: &i32, pool: Pool<Postgres>) -> Result<User, anyhow::Error> {
         let mut conn = pool.acquire().await?;
@@ -30,38 +31,18 @@ impl FromDatabase<i32> for User {
     }
 }
 
-impl User {
-    pub async fn add_user(
-        username: String,
-        password: String,
-        pool: Pool<Postgres>,
-    ) -> Result<Self, anyhow::Error> {
-        let mut conn = pool.acquire().await?;
-        let record = sqlx::query!(
-            r" INSERT INTO Users (username, password_digest) VALUES ($1,crypt($2, gen_salt('bf'))) RETURNING user_id",
-            username,
-            password,
-        ).fetch_one(&mut conn).await?;
-        Ok(User {
-            username: username,
-            user_id: record.user_id,
-            password_digest: (),
-        })
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct Proposal {
     proposal_id: i32,
-    user_id: IndexItem<User, i32>,
-    job_id: IndexItem<Job, i32>,
+    user_id: IndexItem<User>,
+    job_id: IndexItem<Job>,
     proposal: Option<String>,
 }
 
 #[async_trait]
-impl FromDatabase<i32> for Proposal {
+impl FromDatabase for Proposal {
     type ERROR = anyhow::Error;
-
+    type Id = i32;
     async fn build_from_index(id: &i32, pool: Pool<Postgres>) -> Result<Proposal, anyhow::Error> {
         let mut conn = pool.acquire().await?;
         let row = sqlx::query!("select * from Proposals where proposal_id = $1", id,)
@@ -69,16 +50,16 @@ impl FromDatabase<i32> for Proposal {
             .await?;
         Ok(Proposal {
             proposal_id: row.proposal_id,
-            user_id: row.user_id.into(),
-            job_id: row.job_id.into(),
+            user_id: IndexItem::<User>::Id(row.user_id),
+            job_id: IndexItem::<Job>::Id(row.job_id),
             proposal: row.proposal,
         })
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct Job {
-    job_id: i32,
+pub struct Job<T = i32> {
+    job_id: T,
     title: String,
     website: String,
     description: String,
@@ -88,12 +69,13 @@ pub struct Job {
 }
 
 #[async_trait]
-impl FromDatabase<i32> for Job {
+impl FromDatabase for Job {
     type ERROR = anyhow::Error;
+    type Id = i32;
 
     async fn build_from_index(id: &i32, pool: Pool<Postgres>) -> Result<Job, anyhow::Error> {
         let mut conn = pool.acquire().await?;
-        let row = sqlx::query!("select * from jobs where job_id = $1", id,)
+        let row = sqlx::query!("select * from jobs where job_id = $1", id)
             .fetch_one(&mut conn)
             .await?;
         Ok(Job {
@@ -110,116 +92,126 @@ impl FromDatabase<i32> for Job {
 
 #[derive(Debug)]
 pub struct PendingJob {
-    job_id: IndexItem<Job, i32>,
-    user_id: IndexItem<User, i32>,
-    proposal_id: IndexItem<Proposal, i32>,
+    job_id: IndexItem<Job>,
+    user_id: IndexItem<User>,
+    proposal_id: IndexItem<Proposal>,
 }
 struct Database {
     pool: Pool<Postgres>,
 }
 
-// impl Database {
-//     fn new(pool: Pool<Postgres>) -> Self {
-//         Database { pool: pool }
-//     }
+impl Database {
+    fn new(pool: Pool<Postgres>) -> Self {
+        Database { pool: pool }
+    }
 
-//     async fn build_from_db(
-//         &self,
-//         username: String,
-//         password: String,
-//     ) -> Result<User, anyhow::Error> {
-//         let mut conn: sqlx::pool::PoolConnection<Postgres> = self.pool.acquire().await?;
-//         let record = sqlx::query!(
-//             r"SELECT user_id FROM Users WHERE username = $1 AND password_digest = crypt($2, password_digest)",
-//             username,
-//             password,
-//         )
-//         .fetch_one(&mut conn)
-//         .await
-//         .map_err(|_| {
-//             UserBuilderError::ValidationError(format!("Couldn't find user: {:?}", username))
-//         })?;
+    pub async fn add_user(
+        username: String,
+        password: String,
+        pool: Pool<Postgres>,
+    ) -> Result<User, anyhow::Error> {
+        let mut conn = pool.acquire().await?;
+        let record = sqlx::query!(
+            r" INSERT INTO Users (username, password_digest) VALUES ($1,crypt($2, gen_salt('bf'))) RETURNING user_id",
+            username,
+            password,
+        ).fetch_one(&mut conn).await?;
+        Ok(User {
+            username: username,
+            user_id: record.user_id,
+            password_digest: (),
+        })
+    }
 
-//         // verify password
+    async fn get_user(&self, username: String, password: String) -> Result<User, anyhow::Error> {
+        let mut conn: sqlx::pool::PoolConnection<Postgres> = self.pool.acquire().await?;
+        let record = sqlx::query!(
+            r"SELECT user_id FROM Users WHERE username = $1 AND password_digest = crypt($2, password_digest)",
+            username,
+            password,
+        )
+        .fetch_one(&mut conn)
+        .await?;
 
-//         let verified_user = User {
-//             user_id: record.user_id,
-//             username,
-//             password: (),
-//         };
+        // verify password
 
-//         Ok(verified_user)
-//     }
+        let verified_user = User {
+            user_id: record.user_id,
+            username,
+            password_digest: (),
+        };
 
-//     async fn add_job(&self) -> Result<Job, anyhow::Error> {
-//         let mut conn: sqlx::pool::PoolConnection<Postgres> = pool.acquire().await?;
-//         let job = self.build()?;
-//         let record = sqlx::query!(
-//             r"INSERT INTO Jobs
-//         (title, website, description, budget, hourly, post_url)
-//         VALUES ($1, $2, $3, $4, $5, $6) RETURNING job_id",
-//             job.title.clone(),
-//             job.website.clone(),
-//             job.description.clone(),
-//             job.budget.clone(),
-//             job.hourly.clone(),
-//             job.post_url.clone()
-//         )
-//         .fetch_one(&mut conn)
-//         .await?;
+        Ok(verified_user)
+    }
 
-//         Ok(Job {
-//             job_id: record.job_id,
-//             title: job.title,
-//             website: job.website,
-//             description: job.description,
-//             budget: job.budget,
-//             hourly: job.hourly,
-//             post_url: job.post_url,
-//         })
-//     }
-//     async fn get_user_denied_jobs(
-//         &self,
-//         username: &str,
-//     ) -> Result<Vec<(Job, Proposal)>, anyhow::Error> {
-//         let mut conn = self.pool.acquire().await?;
+    async fn add_job(&self) -> Result<Job, anyhow::Error> {
+        let mut conn: sqlx::pool::PoolConnection<Postgres> = self.pool.acquire().await?;
+        let record = sqlx::query!(
+            r"INSERT INTO Jobs
+        (title, website, description, budget, hourly, post_url)
+        VALUES ($1, $2, $3, $4, $5, $6) RETURNING job_id",
+            job.title.clone(),
+            job.website.clone(),
+            job.description.clone(),
+            job.budget.clone(),
+            job.hourly.clone(),
+            job.post_url.clone()
+        )
+        .fetch_one(&mut conn)
+        .await?;
 
-//         static query: &str = r#"
-//         SELECT j.*, p.*
-//         FROM Jobs j
-//         JOIN DecidedJobs d ON j.job_id = d.job_id
-//         JOIN Proposals p ON d.proposal_id = p.proposal_id
-//         JOIN Users u ON d.user_id = u.user_id
-//         WHERE u.username = $1 AND d.accepted = false;
-//         "#;
+        Ok(Job {
+            job_id: record.job_id,
+            title: job.title,
+            website: job.website,
+            description: job.description,
+            budget: job.budget,
+            hourly: job.hourly,
+            post_url: job.post_url,
+        })
+    }
+    async fn get_user_denied_jobs(
+        &self,
+        username: &str,
+    ) -> Result<Vec<(Job, Proposal)>, anyhow::Error> {
+        let mut conn = self.pool.acquire().await?;
 
-//         let rows = sqlx::query(query)
-//             .bind(username)
-//             .fetch_all(&mut conn)
-//             .await?;
+        static query: &str = r#"
+        SELECT j.*, p.*
+        FROM Jobs j
+        JOIN DecidedJobs d ON j.job_id = d.job_id
+        JOIN Proposals p ON d.proposal_id = p.proposal_id
+        JOIN Users u ON d.user_id = u.user_id
+        WHERE u.username = $1 AND d.accepted = false;
+        "#;
 
-//         let denied_jobs = rows
-//             .into_iter()
-//             .map(|row| {
-//                 (
-//                     Job {
-//                         job_id: row.get(0),
-//                         title: row.get(1),
-//                         website: row.get(2),
-//                         description: row.get(3),
-//                         budget: row.get(4),
-//                         hourly: row.get(5),
-//                         post_url: row.get(6),
-//                     },
-//                     Proposal {
-//                         proposal_id: row.get(7),
-//                         user_id: row.get(8),
-//                         job_id: row.get(9),
-//                     },
-//                 )
-//             })
-//             .collect();
+        let rows = sqlx::query(query)
+            .bind(username)
+            .fetch_all(&mut conn)
+            .await?;
 
-//         Ok(denied_jobs)
-//     }
-// }
+        let denied_jobs = rows
+            .into_iter()
+            .map(|row| {
+                (
+                    Job {
+                        job_id: row.get(0),
+                        title: row.get(1),
+                        website: row.get(2),
+                        description: row.get(3),
+                        budget: row.get(4),
+                        hourly: row.get(5),
+                        post_url: row.get(6),
+                    },
+                    Proposal {
+                        proposal_id: row.get(7),
+                        user_id: row.get(8),
+                        job_id: row.get(9),
+                    },
+                )
+            })
+            .collect();
+
+        Ok(denied_jobs)
+    }
+}
